e56896fff50eb79f4089a0e7f935cc1e58f653c9
Sagar Tomar <sagartomar97@gmail.com>
2022-03-05 21:35:44 +0530

    Added function to read RESP Bulk strings along with test

--- app/handler.go
+++ app/handler.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 const PONG string = "PONG"
 
 type Handler struct {
+    reader *bufio.Reader
     writer io.Writer
 }
 
@@ -18,4 +22,30 @@ func (h *Handler) Ping() error {
 func (h *Handler) Echo(message string) error {
     _, err := h.writer.Write([]byte(ConvertToRESPBulkString(message)))
     return err
+}
+
+func (h *Handler) ReadRESPBulkString() (string, error) {
+    input, err := h.reader.ReadString('\n')
+    if err != nil {
+        return "", err
+    }
+    input = RemoveLF(input)
+    input = RemoveCR(input)
+    if input[0] != '$' {
+        return "", fmt.Errorf("Expected '$' but received %c as first character", input[0])
+    }
+    length, err := strconv.Atoi(input[1:])
+    if err != nil {
+        return "", err
+    }
+    input, err = h.reader.ReadString('\n')
+    if err != nil {
+        return "", err
+    }
+    input = RemoveLF(input)
+    input = RemoveCR(input)
+    if len(input) != length {
+        return "", fmt.Errorf("Expected length %d but received length %d", length, len(input))
+    }
+    return input, nil
 }
--- app/handler_test.go
+++ app/handler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"testing"
 )
@@ -32,6 +33,48 @@ func TestEcho(t *testing.T) {
     received := buffer.String()
 
     AssertStringEqual(t, received, expected)
+}
+
+func TestReadRESPBulkString(t *testing.T) {
+
+    t.Run("ReadRESPBulkString should process the RESP bulk string and return the correct string", func(t *testing.T) {
+        buffer := bytes.NewBufferString("$7\r\ntesting\r\n")
+        mockHandler := Handler {reader: bufio.NewReader(buffer)}
+        expected := "testing"
+
+        received, err := mockHandler.ReadRESPBulkString()
+
+        AssertNoError(t, err)
+        AssertStringEqual(t, received, expected)
+    })
+
+    t.Run("ReadRESPBulkString should return an error when first character is not '$'", func(t *testing.T) {
+        buffer := bytes.NewBufferString("7\r\ntesting\r\n")
+        mockHandler := Handler {reader: bufio.NewReader(buffer)}
+        
+        received, err := mockHandler.ReadRESPBulkString()
+
+        AssertError(t, err)
+        AssertStringEqual(t, received, "")
+    })
+
+    t.Run("ReadRESPBulkString should return an error when size and string length are not equal", func(t *testing.T) {
+        buffer := bytes.NewBufferString("$4\r\ntest string\r\n")
+        mockHandler := Handler {reader: bufio.NewReader(buffer)}
+
+        received, err := mockHandler.ReadRESPBulkString()
+        
+        AssertError(t, err)
+        AssertStringEqual(t, received, "")
+    })
+
+}
+
+func AssertError(t testing.TB, err error) {
+    t.Helper()
+    if err == nil {
+        t.Error("Expected an error but didn't receive any")
+    }
 }
 
 func AssertNoError(t testing.TB, err error) {

070cd483b52d390a0012010bae74113870377722
Sagar Tomar <sagartomar97@gmail.com>
2022-03-09 18:26:55 +0530

    Removed unused code

--- app/server.go
+++ app/server.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-    "bufio"
-    "strconv"
-    "strings"
 )
 
 const (
@@ -31,117 +28,8 @@ func main() {
             os.Exit(1)
         }   
 
-        // go HandleConnection(conn)
         handler := NewHandler(conn)
         go handler.HandleConnection()
-    }
-}
-
-func HandleConnection(conn net.Conn) {
-    reader := bufio.NewReader(conn)
-    defer conn.Close()
-
-    for {
-        input, err := ProcessInput(reader)
-        if err != nil {
-            break
-        }
-        if input != nil {
-            for _, in := range input {
-                fmt.Println(in)
-            }
-        }
-        if input != nil {
-            ExecuteCommand(input, conn)
-        }
-    }
-}
-
-func ProcessInput(reader *bufio.Reader) ([]string, error) {
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        fmt.Println("Error while reading data:", err.Error())
-        return nil, err
-    }
-    input = RemoveLF(input)
-    input = RemoveCR(input)
-    if input[0] != '*' {
-        fmt.Println("Invalid character:", input[0])
-        return []string {"PING"}, nil
-    }
-    arrayLen, err := strconv.Atoi(input[1 : ])
-    if err != nil {
-        fmt.Println("Error while converting array length to int", err.Error())
-        return nil, nil
-    }
-    arr := make([]string, arrayLen)
-    for i := 0; i < arrayLen; i++ {
-        data := ProcessElement(reader)
-        if data == nil {
-            fmt.Println("Error in reading element")
-            return nil, nil
-        }
-        arr[i] = *data
-    }
-    return arr, nil
-}
-
-func ProcessElement(reader *bufio.Reader) *string {
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        fmt.Println("Error while reading element size:", err.Error())
-        return nil
-    }
-    input = RemoveLF(input)
-    input = RemoveCR(input)
-    if input[0] != '$' {
-        fmt.Println("Invalid character:", input[0])
-        return nil
-    }
-    elementLength, err := strconv.Atoi(input[1 : ])
-    if err != nil {
-        fmt.Println("Error while converting element length to int:", err.Error())
-        return nil
-    }
-    fmt.Println(elementLength)
-    input, err = reader.ReadString('\n')
-    if err != nil {
-        fmt.Println("Error while reading element:", err.Error())
-        return nil
-    }
-    input = RemoveLF(input)
-    input = RemoveCR(input)
-    if len(input) != elementLength {
-        fmt.Printf("Size mismatch: %d != %d\n", elementLength, len(input))
-        return nil
-    }
-    return &input
-}
-
-func ExecuteCommand(input []string, conn net.Conn) {
-    switch(strings.ToUpper(input[0])) {
-    case "PING":
-        Ping(conn)
-    case "ECHO":
-        Echo(conn, input[1 :])
-    }
-}
-
-func Ping(conn net.Conn) {
-    err := WriteToConn(conn, ConvertToRESPSimpleString(PONG))
-    if err != nil {
-        fmt.Println("Error while sending the message:", err.Error())
-    }
-}
-
-func Echo(conn net.Conn, arguments []string) {
-    if len(arguments) != 1 {
-        fmt.Printf("ECHO expects 1 argument but received %d\n", len(arguments))
-        return
-    }
-    err := WriteToConn(conn, ConvertToRESPBulkString(arguments[0]))
-    if err != nil {
-        fmt.Println("Error while sending the message:", err.Error())
     }
 }
 
@@ -157,15 +45,6 @@ func RemoveLF(input string) string {
         return input[0 : len(input) - 1]
     }
     return input
-}
-
-func WriteToConn(conn net.Conn, message string) error {
-    n, err := conn.Write([]byte(message))
-    if err != nil {
-        return err
-    }
-    fmt.Printf("Wrote %d bytes\n", n)
-    return nil
 }
 
 func ConvertToRESPSimpleString(message string) string {
